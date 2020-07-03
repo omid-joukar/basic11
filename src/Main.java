f3ecf7e432b5073e354687efd635f437542ea4e9
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-03 16:00:39 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by KPS on 7/3/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+       double radius = 7.5;
+       double perimiter = 2 * Math.PI*radius;
+       double area = Math.PI*radius*radius;
+        System.out.println("perimiter : "+perimiter);
+        System.out.println("area : "+area);
+
+    }
+}
